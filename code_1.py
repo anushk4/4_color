edc5cb962d10572007120e67c91ce78b33128181
Anushka Srivastava <anushka22086@iiitd.ac.in>
2023-11-18 02:28:52 +0530

    bfs

--- code_1.py
+++ code_1.py
@@ -26,6 +26,8 @@ def plot_color_map(color_map):
     plt.show()
 
 def check_node(x,y,prev,queue,nodes):
+    if x < 0 or y < 0:
+        return
     try:
         if isinstance(nodes[x][y], int) and nodes[x][y] == prev:
             queue.append((x, y))
@@ -33,28 +35,41 @@ def check_node(x,y,prev,queue,nodes):
         pass
 
 def fill_color(i, j, nodes,color):
-    n = 0
     queue = [(i, j)]
     while queue != []:
         p = queue.pop()
-        n += 1
         prev_value = nodes[p[0]][p[1]]
         nodes[p[0]][p[1]] = color
         check_node(p[0],p[1] + 1, prev_value,queue, nodes)
         check_node(p[0],p[1] - 1, prev_value,queue, nodes)
         check_node(p[0] + 1,p[1], prev_value,queue, nodes)
         check_node(p[0] -  1,p[1], prev_value,queue, nodes)
-       
-def check_neighbour_color(x,y,nodes,na_colors):
+
+def check_existing_color(x, y, queue, nodes, na_colors,val,checked):
+    if (x,y) in checked:
+        return
+    if x < 0 or y < 0:
+        return
     try:
-        if isinstance(nodes[x][y],str):
-            na_colors.append(nodes[x][y])
-        if nodes[x][y] == nodes[x][y+1]:
-            check_neighbour_color(x,y+1,nodes,na_colors)
-        if nodes[x][y] == nodes[x+1][y]:
-            check_neighbour_color(x+1,y,nodes,na_colors)
+        if nodes[x][y] == val:
+            queue.append((x,y))
+        else:
+            if isinstance(nodes[x][y],str):
+                na_colors.append(nodes[x][y])
     except:
         pass
+
+def check_neighbour_color(x,y,nodes,na_colors):
+    queue = [(x,y)]
+    checked = []
+    while queue != []:
+        p = queue.pop()
+        val = nodes[p[0]][p[1]]
+        check_existing_color(p[0],p[1] + 1,queue,nodes,na_colors,val,checked)
+        check_existing_color(p[0],p[1] - 1,queue,nodes,na_colors,val,checked)
+        check_existing_color(p[0] + 1,p[1],queue,nodes,na_colors,val,checked)
+        check_existing_color(p[0] - 1,p[1],queue,nodes,na_colors,val,checked)
+        checked.append(p)
  
 def find_unused_color(colors,na_color):
     for i in colors:
@@ -73,17 +88,11 @@ def find_nodes(nodes):
         for j in range(len(nodes[0])):
             if isinstance(nodes[i][j],str):
                 continue
-            check_neighbour_color(i,j+1,nodes,na_colors)
-            check_neighbour_color(i,j-1,nodes,na_colors)
-            check_neighbour_color(i+1,j,nodes,na_colors)
-            check_neighbour_color(i-1,j,nodes,na_colors)
+            check_neighbour_color(i,j,nodes,na_colors)
             na_colors = list(set(na_colors))
             color = find_unused_color(colors,na_colors)
             fill_color(i, j, nodes,color)
             nodes_number += 1
-            for k in nodes:
-                print("".join(list(map(str,k))))
-            print()
             na_colors = []
     print("{} nodes found".format(nodes_number))
             
